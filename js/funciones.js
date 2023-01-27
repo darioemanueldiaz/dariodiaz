d1506fea000f1c59ef6f6b86c6fb1e0c23c09a25
darioemanueldiaz <122326813+darioemanueldiaz@users.noreply.github.com>
2023-01-27 00:03:19 -0300

    primera subida a github

---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+
+/**document.getElementById('boton_email').addEventListener('click',function(){
+    document.getElementById("email").innerHTML = "homerojsimpson@email.com";
+});*/
+
+document.getElementById('boton_name').addEventListener('click',function(){
+    document.getElementById("name").style.display = 'block';
+});
+
+document.getElementById('boton_date').addEventListener('click',function(){
+    document.getElementById("date").style.display = 'block';
+});
+
+document.getElementById('boton_adress').addEventListener('click',function(){
+    document.getElementById("adress").style.display = 'block';
+});
+
+document.getElementById('boton_nationality').addEventListener('click',function(){
+    document.getElementById("nationality").style.display = 'block';
+});
